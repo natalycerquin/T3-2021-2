1d937081d395a6b2737a950f976cbdbc42038d52
Nataly <nataly.cerquin@hotmail.com>
2021-11-05 18:23:00 -0600

    Initial commit

---
+++ T3 2021-2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace T3_2021_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
