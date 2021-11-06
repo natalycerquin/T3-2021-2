ad4631ebce426f64a29f0a1d514280f2559632cf
Nataly <nataly.cerquin@hotmail.com>
2021-11-05 18:27:33 -0600

    Create Nota.cs
    Primier commit 7:30

---
+++ T3 2021-2/Models/Nota.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T3_2021_2.Models
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
